f9ebc1b2997d9ee6bbcf21d4dcac881ca0a340c2
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-11-11 21:43:15 +0900

    create vue file

---
+++ src/pages/moods/create.vue
@@ -0,0 +1,17 @@
+<script>
+    import Vue from 'vue'
+    Vue.createApp({
+    data() {
+        return {
+            targetDate: ''
+        }
+    }
+    }).mount('#target-mood-date')
+</script>
+
+<template>
+    <div id="target-mood-date">
+        <label for="target-date">Choose a time for your appointment:</label>
+        <input v-model="targetDate" type="datetime-local" id="target-date">
+    </div>
+</template>
