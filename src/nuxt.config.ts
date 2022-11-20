f748f0f3ec0b219cf98ac87e8c4d504cd3cd8f92
burobo <boronobu@gmail.com>
2022-11-20 18:25:03 +0900

    node.jsサーバをプロキシとしてCORSのエラーを回避するようにした

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
     nitro: {
         preset: "aws-lambda",
         serveStatic: true,
-    },    
+    },
     publicRuntimeConfig: {
+        API_PROXY_BASE_URL: "/api",
         API_BASE_URL: process.env.API_BASE_URL,
     },
     css: ["bootstrap/dist/css/bootstrap.min.css"],
     pages: true,
-    // ssr: true
 })
