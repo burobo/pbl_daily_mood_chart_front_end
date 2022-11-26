5d001936260c095f45721ffc9cfaef842fb8c71a
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-11-26 11:36:37 +0900

    fix indent

--- src/pages/activities/log.vue
+++ src/pages/activities/log.vue
@@ -1,67 +1,62 @@
 <template>
     <h1>生活記録表</h1>
-    <ag-grid-vue
-      class="ag-theme-alpine"
-      style="height: 500px"
-      :columnDefs="columnDefs"
-      :rowData="rowData"
-      :defaultColDef="defaultColDef"
-      rowSelection="multiple"
-      animateRows="true"
-    >
+    <ag-grid-vue class="ag-theme-alpine" style="height: 500px" :columnDefs="columnDefs" :rowData="rowData"
+        :defaultColDef="defaultColDef" rowSelection="multiple" animateRows="true">
     </ag-grid-vue>
 </template>
 
 <script>
-   import { AgGridVue } from "ag-grid-vue3";  // the AG Grid Vue Component
+import { AgGridVue } from "ag-grid-vue3";  // the AG Grid Vue Component
 
-   import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
-   import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
-   export default {
+export default {
     name: "App",
     components: {
-      AgGridVue,
+        AgGridVue,
     },
     setup() {
-      // Each Column Definition results in one Column.
-      const columnDefs = [
-        { field: "日付" },
-        { field: "気分" },
-        { field: "就寝" },
-        { field: "起床" },
-        { field: "実睡眠" },
-        { field: "他睡眠" },
-        { field: "睡眠計" }
-      ];
+        // Each Column Definition results in one Column.
+        const columnDefs = [
+            { field: "日付" },
+            { field: "気分" },
+            { field: "就寝" },
+            { field: "起床" },
+            { field: "実睡眠" },
+            { field: "他睡眠" },
+            { field: "睡眠計" }
+        ];
 
-      // Example load data from sever
-      const rowData = [
-        { "日付": "11月16日", "気分": "😄", "就寝": "18:16", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月17日", "気分": "😄", "就寝": "21:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月18日", "気分": "😄", "就寝": "21:57", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月19日", "気分": "😢", "就寝": "23:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月20日", "気分": "😄", "就寝": "23:20", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月21日", "気分": "😄", "就寝": "21:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月22日", "気分": "😄", "就寝": "21:57", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月23日", "気分": "😢", "就寝": "23:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
-        { "日付": "11月24日", "気分": "😄", "就寝": "23:20", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" }
-      ]
+        // Example load data from sever
+        const rowData = [
+            { "日付": "11月16日", "気分": "😄", "就寝": "18:16", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月17日", "気分": "😄", "就寝": "21:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月18日", "気分": "😄", "就寝": "21:57", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月19日", "気分": "😢", "就寝": "23:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月20日", "気分": "😄", "就寝": "23:20", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月21日", "気分": "😄", "就寝": "21:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月22日", "気分": "😄", "就寝": "21:57", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月23日", "気分": "😢", "就寝": "23:24", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" },
+            { "日付": "11月24日", "気分": "😄", "就寝": "23:20", "起床": "4:16", "実睡眠": "9:34", "他睡眠": "0:00", "睡眠計": "9:34" }
+        ]
 
-      // DefaultColDef sets props common to all Columns
-      const defaultColDef = {
-        sortable: true,
-        filter: true,
-        flex: 1
-      };
+        // DefaultColDef sets props common to all Columns
+        const defaultColDef = {
+            sortable: true,
+            filter: true,
+            flex: 1
+        };
 
-      return {
-        columnDefs,
-        rowData,
-        defaultColDef
-      };
+        return {
+            columnDefs,
+            rowData,
+            defaultColDef
+        };
     },
-   };
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+</style>
