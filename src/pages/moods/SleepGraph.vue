4d7b22f515181a609e37b3214d6942c027e7555c
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-11-30 07:40:53 +0900

    表の項目修正

---
+++ src/pages/moods/SleepGraph.vue
@@ -0,0 +1,60 @@
+<template>
+    <h1>生活記録表</h1>
+    <ag-grid-vue class="ag-theme-alpine" style="height: 500px" :columnDefs="columnDefs" :rowData="rowData"
+        :defaultColDef="defaultColDef" rowSelection="multiple" animateRows="true">
+    </ag-grid-vue>
+</template>
+
+<script>
+import { AgGridVue } from "ag-grid-vue3";  // the AG Grid Vue Component
+
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+export default {
+    name: "App",
+    components: {
+        AgGridVue,
+    },
+    setup() {
+        // Each Column Definition results in one Column.
+        const columnDefs = [
+            { field: "日付" },
+            { field: "気分" },
+            { field: "実睡眠時間" },
+            { field: "睡眠時間" },
+            { field: "睡眠効率" }
+        ];
+
+        // Example load data from sever
+        const rowData = [
+            { "日付": "11月16日", "気分": "😄", "実睡眠時間": "18:16", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月17日", "気分": "😄", "実睡眠時間": "21:24", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月18日", "気分": "😄", "実睡眠時間": "21:57", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月19日", "気分": "😢", "実睡眠時間": "23:24", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月20日", "気分": "😄", "実睡眠時間": "23:20", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月21日", "気分": "😄", "実睡眠時間": "21:24", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月22日", "気分": "😄", "実睡眠時間": "21:57", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月23日", "気分": "😢", "実睡眠時間": "23:24", "睡眠時間": "04:16", "睡眠効率": "09:34" },
+            { "日付": "11月24日", "気分": "😄", "実睡眠時間": "23:20", "睡眠時間": "04:16", "睡眠効率": "09:34" }
+        ]
+
+        // DefaultColDef sets props common to all Columns
+        const defaultColDef = {
+            sortable: true,
+            filter: true,
+            flex: 1
+        };
+
+        return {
+            columnDefs,
+            rowData,
+            defaultColDef
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
