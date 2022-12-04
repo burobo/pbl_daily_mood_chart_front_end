791b5fddad0fecf62c92cbff08e239bc686b617e
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-12-04 11:57:53 +0900

    睡眠表とグラフのタブ切り替えを作成

--- src/pages/moods/SleepGraph.vue
+++ src/pages/moods/SleepGraph.vue
@@ -1,14 +1,28 @@
 <template>
-    <h1>睡眠分析表</h1>
-    <div class="border mb-5"></div>
-    <div class="mb-2">
-        <small class="me-3"><strong>※実睡眠時間</strong>：体が完全に安静な状態の時間</small>
-        <small class="me-3"><strong>※睡眠時間</strong>：ベッドにいた時間</small>
-        <small><strong>※睡眠効率</strong>：実睡眠時間 / 睡眠時間×100</small>
+    <ul class="tabnav">
+        <li @click="isActive = 'table'" :class="{ 'active': isActive === 'table' }">
+            睡眠表
+        </li>
+        <li @click="isActive = 'graph'" :class="{ 'active': isActive === 'graph' }">
+            睡眠グラフ
+        </li>
+    </ul>
+    <div v-if="isActive === 'table'">
+        <h1>睡眠表</h1>
+        <div class="border mb-5"></div>
+        <div class="mb-2">
+            <small class="me-3"><strong>※実睡眠時間</strong>：体が完全に安静な状態の時間</small>
+            <small class="me-3"><strong>※睡眠時間</strong>：ベッドにいた時間</small>
+            <small><strong>※睡眠効率</strong>：実睡眠時間 / 睡眠時間×100</small>
+        </div>
     </div>
-    <ag-grid-vue class="ag-theme-alpine" style="height: 500px" :columnDefs="columnDefs" :rowData="rowData"
-        :defaultColDef="defaultColDef" rowSelection="multiple" animateRows="true">
-    </ag-grid-vue>
+    <div v-else-if="isActive === 'graph'">
+        <h1>睡眠グラフ</h1>
+        <div class="border mb-5"></div>
+        <ag-grid-vue class="ag-theme-alpine" style="height: 500px" :columnDefs="columnDefs" :rowData="rowData"
+            :defaultColDef="defaultColDef" rowSelection="multiple" animateRows="true">
+        </ag-grid-vue>
+    </div>
 </template>
 
 <style>
@@ -20,6 +34,25 @@
 .ag-header-cell:not(:last-child) {
     border-right: solid 0.1px;
 }
+
+.tabnav {
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+}
+
+.tabnav li {
+    cursor: pointer;
+    width: 50%;
+    background: #f2f3f5;
+    padding: 10px;
+    text-decoration: none;
+}
+
+.tabnav li.active {
+    background: #3cc0d9;
+    color: #fff;
+}
 </style>
 
 <script>
@@ -27,6 +60,8 @@ import { AgGridVue } from "ag-grid-vue3";  // the AG Grid Vue Component
 
 import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
 import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+
+import { ref } from 'vue';
 
 export default {
     name: "SleepGraph",
@@ -37,30 +72,30 @@ export default {
         // Each Column Definition results in one Column.
         const columnDefs = [
             { field: "日付", filter: false, minWidth: 90 },
-            { field: "0", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "1", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "2", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "3", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "4", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "5", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "6", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "7", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "8", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "9", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "blue", fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "10", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "11", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "12", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "13", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "14", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "15", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "16", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "17", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "18", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "19", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "20", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "21", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "22", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null },
-            { field: "23", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: 'blue', fontSize: 0, borderRight: "dotted white" } : null }
+            { field: "0", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "1", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "2", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "3", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "4", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "5", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "6", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "7", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "8", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "9", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "10", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "11", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "12", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "13", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "14", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "15", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "16", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "17", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "18", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "19", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "20", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "21", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "22", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null },
+            { field: "23", filter: false, sortable: false, cellStyle: params => params.value === "true" ? { backgroundColor: "#0b3769", fontSize: 0, borderRight: "dotted white" } : null }
         ];
 
         // Example load data from sever
@@ -83,10 +118,13 @@ export default {
             flex: 1
         };
 
+        const isActive = ref('table')
+
         return {
             columnDefs,
             rowData,
-            defaultColDef
+            defaultColDef,
+            isActive
         };
     },
 };
