9aa050d527e67c87c135a50bc519d12da355b681
NanakoSawamura <n.k.0213.0529@gmail.com>
2022-12-22 20:59:16 +0900

    凡例のデザイン作成

--- src/pages/activities/log.vue
+++ src/pages/activities/log.vue
@@ -2,6 +2,23 @@
   <h1>生活記録表</h1>
   <div class="border mb-5"></div>
   <div class="mb-3">
+    <table>
+      <tr>
+        <th style="width: 40px;">凡例</th>
+        <td class="sleep-bg"></td>
+        <td>睡眠（自動計測）</td>
+        <td class="exercise-bg"></td>
+        <td style="width: 40px;">運動</td>
+        <td class="commute-bg"></td>
+        <td style="width: 40px;">通勤</td>
+        <td class="bathe-bg"></td>
+        <td style="width: 40px;">入浴</td>
+        <td class="commute-hospital-bg"></td>
+        <td>通院</td>
+      </tr>
+    </table>
+  </div>
+  <div class="mb-3">
     <small class="me-3"><strong>※実睡眠時間</strong>：体が完全に安静な状態の時間</small>
     <small class="me-3"><strong>※睡眠時間</strong>：ベッドにいた時間</small>
     <small><strong>※睡眠効率</strong>：実睡眠時間 / 睡眠時間×100</small>
@@ -28,6 +45,32 @@
 </template>
 
 <style>
+.sleep-bg {
+  background-color: #0b3769;
+  padding: 20px 25px;
+  margin: 0 3px;
+}
+
+.exercise-bg {
+  background-color: #F45656;
+  padding: 20px 25px;
+}
+
+.commute-bg {
+  background-color: #ffc003;
+  padding: 20px 25px;
+}
+
+.bathe-bg {
+  background-color: #3BC1D9;
+  padding: 20px 25px;
+}
+
+.commute-hospital-bg {
+  background-color: #34a853;
+  padding: 20px 25px;
+}
+
 .tabnav {
   display: flex;
   list-style-type: none;
